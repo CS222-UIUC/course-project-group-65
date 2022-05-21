228d9e8dfbc91af58384c555233903e7c7cdb5a5
Saloni Vaishnav <inolasv@gmail.com>
2022-05-21 18:29:49 -0500

    set up xcode

---
+++ RoomHub/RoomHub/RoomHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomHubApp.swift
+//  RoomHub
+//
+//  Created by Saloni Vaishnav on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct RoomHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
