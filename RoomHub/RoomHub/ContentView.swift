bc9d79c89a627ed748fa189465d42c8183de9dfd
nikhilapuppala <puppala4@illinois.edu>
2022-05-24 15:21:00 -0500

    fixed FirebaseAuth error and removed pods

--- RoomHub/RoomHub/ContentView.swift
+++ RoomHub/RoomHub/ContentView.swift
@@ -7,10 +7,29 @@
 
 import SwiftUI
 import FirebaseAuth
+import simd
 
-class AppViewModel: ObservedObject {
+class AppViewModel: ObservableObject {
     let auth = Auth.auth()
     
+    var isSignedIn: Bool {
+        return auth.currentUser != nil
+    }
+    func signIn(email: String, password: String) {
+        auth.signIn(withEmail: email, password: password) { result, error in
+            guard result != nil, error == nil else {
+                return
+            }
+        // success
+        }
+    }
+    func signUp(email: String, password: String) {
+        auth.createUser(withEmail: email, password: password) { result, error in
+            guard result != nil, error == nil else {
+                return
+            }
+        }
+    }
 }
 
 struct ContentView: View {
